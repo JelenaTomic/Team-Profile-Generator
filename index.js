6c972bcab75be45237b2b49480da30df0c50fb59
JelenaTomic <tomic.elena09@gmail.com>
2022-08-15 16:44:38 -0500

    making prompt for manager

--- index.js
+++ index.js
@@ -4,3 +4,40 @@ const fs = require ('fs')
 const Manager = require('./lib/manager');
 const Engineer = require ('./lib/engineer');
 const Intern = require ('./lib/intern');
+
+let team = {
+    manager : null,
+    engineers : [],
+    interns : []
+}
+// mng
+function init(){
+    inquirer.Prompt([
+        {
+            type: "input",
+            name : "mnName",
+            message : "What is the managers name"
+        },
+        {
+            type : "input",
+            name : "mnId",
+            message: "What is the managers employee ID number"
+        },
+        {
+            type : "input",
+            name : "mnEmail",
+            message: "What is the managers email?"
+
+        },
+        {
+            type : "input",
+            name : "mnOfficeNumber",
+            message: "What is the managers office number?"
+
+        }
+    
+    ]).then(answer =>{
+        team.manager = new Manager(answer.mnName, answer.mnId, answer.mnEmail, answer.mnOfficeNumber);
+        addTeamMember();
+    })
+}
