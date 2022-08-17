a56a616c56ff90b91f13cac13945872d531b7d96
JelenaTomic <tomic.elena09@gmail.com>
2022-08-16 22:18:20 -0500

    made test for the engineer

---
+++ __tests__/engineer.test.js
@@ -0,0 +1,21 @@
+const Engineer = require("../lib/engineer");
+
+test('creates an Engineer object', () => {
+    const engineer = new Engineer('Chandler', 2, 'chandler@gmail.com', 'https://github.com/JelenaTomic');
+    expect(engineer.name).toBe('Chandler');
+    expect(engineer.id).toEqual(expect.any(Number));
+    expect(engineer.email).toBe('chandler@gmail.com');
+    expect(engineer.GitHub).toBe('https://github.com/JelenaTomic');
+ 
+})
+  
+test("Test school", () => {
+    const engineer = new Engineer('Chandler', 2, 'chandler@gmail.com', 'https://github.com/JelenaTomic');
+    expect(engineer.getGitHub()).toEqual(expect.any(String));
+  
+});
+
+test("Test role", () => {
+    const engineer = new Engineer('Chandler', 2, 'chandler@gmail.com', 'https://github.com/JelenaTomic');
+    expect(engineer.getRole()).toBe('Engineer');
+  }); 
