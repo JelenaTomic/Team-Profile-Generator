bb6a5d5142dd79df6a705863541c633d065d70ff
JelenaTomic <tomic.elena09@gmail.com>
2022-08-16 22:10:01 -0500

    made test for employee

---
+++ __tests__/employee.test.js
@@ -0,0 +1,32 @@
+const Employee = require("../lib/employee");
+
+test('creates a Employee object', () => {
+  const employee = new Employee('Rachel', 1, 'rachel@gmail.com');
+
+  expect(employee.name).toBe('Rachel');
+  expect(employee.id).toEqual(expect.any(Number));
+  expect(employee.email).toBe('rachel@gmail.com');
+})
+
+test("Test name", () => {
+    const employee = new Employee('Rachel', 1, 'rachel@gmail.com');
+    expect(employee.getName()).toEqual(expect.any(String));
+
+});
+
+test("Test role", () => {
+    const employee = new Employee('Rachel', 1, 'rachel@gmail.com');
+    expect(employee.getId()).toEqual(expect.any(Number));
+  
+});
+
+test("Test school", () => {
+    const employee = new Employee('Rachel', 1, 'rachel@gmail.com');
+    expect(employee.getEmail()).toEqual(expect.any(String));
+  
+});
+
+test("Test role", () => {
+    const employee = new Employee('Rachel', 1, 'rachel@gmail.com');
+    expect(employee.getRole()).toBe('Employee');
+  }); 
