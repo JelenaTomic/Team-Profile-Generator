1ee2b93c304a4d63fc2a053758a9c7e178e9841d
JelenaTomic <tomic.elena09@gmail.com>
2022-08-16 21:50:14 -0500

    added function that generate engineer in template.js

--- src/template.js
+++ src/template.js
@@ -14,7 +14,7 @@ generatePage = team => {
     );
     return outputHtml.join("");
 }
-
+// function that generate manager
 generateManager = manager => {
     return `
         <div class="card employee-card">
@@ -31,5 +31,23 @@ generateManager = manager => {
           </div>
      </div>
      `;
-}
+};
+// function that generate engineer
+const generateEngineer = engineer => {
+    return `
+        <div class="card employee-card">
+          <div class="card-header bg-primary text-white">
+              <h2 class="card-title">${engineer.getName()}</h2>
+              <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
+          </div>
+          <div class="card-body">
+               <ul class="list-group">
+                   <li class="list-group-item">ID: ${engineer.getId()}</li>
+                   <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+                   <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.getGithub()}" target="_blank" rel="noopener noreferrer">${engineer.getGithub()}</a></li>
+               </ul>
+          </div>
+      </div>
+    `;
+};
 
